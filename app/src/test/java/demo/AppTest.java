7fce4b951853d042eb623da20a0f70a39fd823bd
partha <parthasaradhi.e1@gmail.com>
2023-07-01 02:12:30 +0530

    demo

---
+++ app/src/test/java/demo/AppTest.java
@@ -0,0 +1,16 @@
+package demo;
+
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertThat;
+
+/**
+ * AppTest
+ */
+public class AppTest {
+
+    void test(){
+        assertThat(null, null, null);
+        asserThrows(null,null);
+    }
+}
